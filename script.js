a7c6d38b739a892bad59098761930f493646e715
Faithhhhh1 <faithlives01@gmail.com>
2026-06-21 17:54:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+alert("JS is loading!");
 
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+ctx.fillStyle = "hotpink";
+ctx.fillRect(100, 100, 200, 200);
