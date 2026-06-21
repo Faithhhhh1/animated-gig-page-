a4c767e47ad42ca61256d643241663fa7aaeb4b7
Faithhhhh1 <faithlives01@gmail.com>
2026-06-21 15:39:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,39 +1,28 @@
 const svg = document.querySelector(".heart");
 
-// Number of text layers
-const layers = 30;
+if (!svg) {
+  console.log("SVG not found");
+} else {
+  for (let i = 0; i < 30; i++) {
+    const text = document.createElementNS(
+      "http://www.w3.org/2000/svg",
+      "text"
+    );
 
-for (let i = 0; i < layers; i++) {
-  // Create <text>
-  const text = document.createElementNS(
-    "http://www.w3.org/2000/svg",
-    "text"
-  );
+    text.setAttribute("class", "love");
 
-  text.setAttribute("class", "love horizontal");
+    const textPath = document.createElementNS(
+      "http://www.w3.org/2000/svg",
+      "textPath"
+    );
 
-  // Used by CSS animation-delay
-  text.style.setProperty("--i", i);
+    textPath.setAttribute("href", "#heartPath");
+    textPath.setAttribute("startOffset", `${i}%`);
 
-  // Create <textPath>
-  const textPath = document.createElementNS(
-    "http://www.w3.org/2000/svg",
-    "textPath"
-  );
+    textPath.textContent =
+      " I love you I love you I love you I love you ";
 
-  // Attach text to the heart path
-  textPath.setAttribute("href", "#heartPath");
-
-  // Slightly offset each line
-  textPath.setAttribute(
-    "startOffset",
-    `${i * 0.35}%`
-  );
-
-  // Repeat text around the heart
-  textPath.textContent =
-    " I love you  I love you  I love you  I love you  I love you  I love you ";
-
-  text.appendChild(textPath);
-  svg.appendChild(text);
+    text.appendChild(textPath);
+    svg.appendChild(text);
+  }
 }
